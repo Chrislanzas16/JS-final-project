7a9938d86c3aeff33ccdf368933a49a48fc5dfca
Chris <chrislanzas89@outlook.com>
2025-07-23 15:32:32 -0400

    Added filtering options

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 const movieResult = document.querySelector(".movieResults")
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
+document.getElementById('filter').addEventListener("change", filterMovies);
+let currentmovies = [];
 
-
-
-
-searchInput.addEventListener("keydown", function (event) {
+searchInput.addEventListener("keydown", async function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
-        movieLookup(searchInput.value)
+        currentmovies = await movieLookup(searchInput.value)
     }
 })
 
@@ -19,10 +18,10 @@ searchInput.addEventListener("input", function (){
     }
 })
 
-searchButton.addEventListener("click", function (){
+searchButton.addEventListener("click", async function (){
     const searchTerm = searchInput.value.trim ();
     if (searchTerm) {
-        movieLookup(searchTerm)
+       currentmovies = await movieLookup(searchTerm)
     }
 })
 
@@ -33,6 +32,7 @@ async function movieLookup(search) {
       try {
       const movieTitle = await fetch (`https://www.omdbapi.com/?apikey=9c546bc8&s=${search}`);
       const movieData =  await movieTitle.json();
+     
 
    if (movieData.Response === "True") {
     const movies = movieData.Search;
@@ -45,14 +45,17 @@ async function movieLookup(search) {
 <br><strong>Year:</strong>
 ${movie.Year}<br> </div>`)
 .join("");
+return movies;
    } else {
     movieResult.innerHTML = `<p>No Results Found</p>`;
    }
+   return [];
    }   catch (error) {
     movieResult.innerHTML = `<p>Error Fetching Movies</p>`;
     console.error (error);
    } finally {
   spinner.classList.add("hidden");
+
    
 }};
 
@@ -60,5 +63,34 @@ async function main () {
    const movieTitle = await fetch (`https://www.omdbapi.com/?apikey=9c546bc8&s=fast`);
    const movieData =  await movieTitle.json();
    const movies = movieData.Search;
-    console.log(movies)
+  
 }
+
+function filterMovies(event) { 
+      const filter = event.target.value;
+      let filtered = currentmovies.slice(0, 6)
+
+      if (filter === "Oldest_To_Newest")
+        filtered.sort ((a, b) => a.Year - b.Year)
+    else if (filter === "Newest_To_Oldest") {
+        filtered.sort ((a, b) => b.Year - a.Year)
+    }
+movieResult.innerHTML = filtered.map((movie) => `
+  <div class="search_result">
+ <img  src= "${movie.Poster}"width=200px<br>
+
+<br><b>Title:</b>
+ ${movie.Title}
+<br><strong>Year:</strong>
+${movie.Year}<br> </div>`)
+.join("");
+      }
+
+
+       
+
+
+
+     
+  
+    
